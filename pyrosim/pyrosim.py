a4f0e15c1cdebe31c0b5f467d7f20dd6f92c14f5
Ajuogaaz <brianlinus1753@gmail.com>
2023-01-26 22:43:13 -0600

    done

--- pyrosim/pyrosim.py
+++ pyrosim/pyrosim.py
@@ -46,6 +46,8 @@ def Get_Touch_Sensor_Value_For_Link(linkName):
 
     touchValue = -1.0
 
+    print(linkNamesToIndices, linkName)
+
     desiredLinkIndex = linkNamesToIndices[linkName]
 
     pts = p.getContactPoints()
